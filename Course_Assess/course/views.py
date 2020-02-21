b5dc8285f56ddc4bfb3c3cdf428c466117fa4e3c
kjh03160 <kjh03160@likelion.org>
2020-02-22 00:43:03 +0900

    평가 댓글 중복 불허 구현 완료, post_detail css 작업중

--- Course_Assess/course/views.py
+++ Course_Assess/course/views.py
@@ -3,7 +3,7 @@ from .models import Course, Assessment
 from .crwal import crwal_Table
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -19,27 +19,16 @@ def course_list(request):
 
 def post_view(request, name, prof):
     course = Course.objects.get(name=name, prof=prof)
-    # print(course.comments)
-    course_name = course.name
-    course_prof = course.prof
-    course_dept = course.dept
-    course_year = course.year
-    course_stars = course.stars
     context = {
-        'name' : course_name,
-        'prof' : course_prof,
-        'dept' : course_dept,
-        'year' : course_year,
-        'stars' : course_stars,
-        'post' : course,
+        'course' : course,
     }
     return render(request, 'post_detail.html', context)
 
 
-def search_by_prof(request):
+def search(request):
     if request.method == "POST":
-        prof = request.POST["prof"]
-        courses = Course.objects.filter(prof__contains=prof).order_by('year')
+        q = request.POST["q"]
+        courses = Course.objects.filter(Q(prof__contains=q) | Q(name__contains=q)).order_by('year')
         if len(courses) != 0:
             return render(request, 'post.html', {'courses' : courses})
         else:
@@ -47,28 +36,18 @@ def search_by_prof(request):
     else:
         return redirect('/')
 
-def search_by_course(request):
-    if request.method == "POST":
-        name = request.POST["course"]
-        courses = Course.objects.filter(name__contains=name).order_by('year')
-        if len(courses) != 0:
-            return render(request, 'post.html', {'courses' : courses})
-        else:
-            return render(request, 'post.html', {'error':'검색 결과가 없습니다'})
-    else:
-        return redirect('/')
 
 def newreply(request):
     if request.method == 'POST':
+        parse = request.POST['blog']
+        prof = parse.split('+')[1]
+        name = parse.split('+')[0]
+        course = Course.objects.get(name = name, prof = prof)
         try:
-            parse = request.POST['blog']
-            prof = parse.split('+')[1]
-            name = parse.split('+')[0]
-            objs = Assessment.objects.get(post=request.user).post
+            objs = Assessment.objects.get(post=request.user, course=course).post
         except:
             comment = Assessment()
             comment.contents = request.POST['comment_body']
-            course = Course.objects.get(name = name, prof = prof) 
             comment.course = course                
             comment.post = request.user
             comment.star = request.POST['rating']
@@ -82,11 +61,15 @@ def newreply(request):
             else:
                 course.stars = int(request.POST['rating']) 
             course.save()
-            return redirect('/posts/'+ comment.course.name + '/' + comment.course.prof, {'course' : course})
+            return redirect('/posts/'+ comment.course.name + '/' + comment.course.prof)
 
         else:
             print(1)
-            return redirect('/posts/'+ name+ '/' + prof, {'error' : '이미 강의평을 등록하셨습니다!'})
+            context = {
+                'course' : course,
+                'error' : '이미 강의평을 등록하셨습니다!'
+                }
+            return render(request, 'post_detail.html', context)
     else :
             return redirect('/posts') # 홈으로
 
